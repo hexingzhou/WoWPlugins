3972dfaa556579428ba414f2d6bc43780b2cf2f4
Fat walking <hexingzhou@gmail.com>
2022-06-17 00:39:09 +0800

    Fix SL.checker function

--- WeakAuras_Scripts/base/Init.lua
+++ WeakAuras_Scripts/base/Init.lua
@@ -1,12 +1,14 @@
-local SLChecker = {}
-function SLChecker:run(config, check)
+local SL = aura_env
+
+SL.checker = {}
+function SL.checker:run(config, check)
     if not config or not config.name then
         return false
     end
     self[config.name] = self[config.name] or {}
     local env = self[config.name]
     if config.enable then
-        local count = config.count or 10
+        local count = config.count or 100
         env.count = env.count or 0
         if env.count < count then
             env.count = env.count + 1
