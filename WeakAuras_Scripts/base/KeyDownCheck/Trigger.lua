bf70388d85e3b7229b8b61de3c22cecaf9be3584
Fat walking <hexingzhou@gmail.com>
2022-06-17 23:27:43 +0800

    Update Trigger.lua

--- WeakAuras_Scripts/base/KeyDownCheck/Trigger.lua
+++ WeakAuras_Scripts/base/KeyDownCheck/Trigger.lua
@@ -6,7 +6,7 @@ function(allstates, event, id, keys)
     elseif event == "SL_KEYDOWN_UNREGISTER" then
         config.keydown:unregister(id)
     end
-    if config.keydown.ids and #config.keydown.ids > 0 then
+    if config.keydown.ids and next(config.keydown.ids) then
         config:start(config.keydown)
     else
         config:stop(config.keydown)
