8c6575ea6c87565b62782355ebc21bf1c396a5db
hexingzhou <hexingzhou@gmail.com>
2022-06-18 22:25:50 +0800

    Support array to find key.

--- WeakAuras_Scripts/FindKeyAndKeyDown.lua
+++ WeakAuras_Scripts/FindKeyAndKeyDown.lua
@@ -24,32 +24,37 @@ function(allstates, event, ...)
         end
 
     elseif event == "SL_KEYBINDING" then
-        local spellID = 0 -- TODO: Set spellID
+        local spellIDs = {} -- TODO: Set spellIDs
         local type = 1 -- For spell, type = 1
-        
+
         local id, key, keys = ...
 
         -- This WA has not loaded.
         if not id then
-            WeakAuras.ScanEvents("SL_KEYBINDING_REGISTER", spellID, type)
-        end
-
-        if id == spellID then
-            allstates["KEY_BINDING"] = {
-                show = true,
-                changed = true,
-                name = key,
-                keyDown = aura_env.keyDown
-            }
-            aura_env.key = key
-            aura_env.keys = keys
-
-            if keys and next(keys) then
-                WeakAuras.ScanEvents("SL_KEYDOWN_REGISTER", aura_env.id, aura_env.keys)
-            else
-                WeakAuras.ScanEvents("SL_KEYDOWN_UNREGISTER", aura_env.id)
+            for i = 1, #spellIDs do
+                WeakAuras.ScanEvents("SL_KEYBINDING_REGISTER", spellIDs[i], type)
             end
         end
+
+        for i = 1, #spellIDs do
+            if id == spellIDs[i] then
+                allstates["KEY_BINDING"] = {
+                    show = true,
+                    changed = true,
+                    name = key,
+                    keyDown = aura_env.keyDown
+                }
+                aura_env.key = key
+                aura_env.keys = keys
+
+                if keys and next(keys) then
+                    WeakAuras.ScanEvents("SL_KEYDOWN_REGISTER", aura_env.id, aura_env.keys)
+                else
+                    WeakAuras.ScanEvents("SL_KEYDOWN_UNREGISTER", aura_env.id)
+                end
+            end
+        end
+
         return true
     end
 
