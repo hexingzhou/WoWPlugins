487cf63a490207a03a707a8454d49ab7f94f7c66
Fat walking <hexingzhou@gmail.com>
2022-06-19 12:08:24 +0800

    Update FindKeyAndKeyDown.lua

--- WeakAuras_Scripts/FindKeyAndKeyDown.lua
+++ WeakAuras_Scripts/FindKeyAndKeyDown.lua
@@ -27,31 +27,42 @@ function(allstates, event, ...)
         local ids = {} -- TODO: Set spell or item id
         local type = 1 -- For spell, type = 1. For item, type = 2
 
+        local checkID
+        if type == 1 then
+            for i = 1, #ids do
+                if IsPlayerSpell(ids[i]) then
+                    checkID = ids[i]
+                    break
+                end
+            end
+        elseif type == 2 then
+            checkID = ids[1]
+        end
+        if not checkID then
+            return false
+        end
+
         local id, key, keys = ...
 
         -- This WA has not loaded.
         if not id then
-            for i = 1, #ids do
-                WeakAuras.ScanEvents("SL_KEYBINDING_REGISTER", ids[i], type)
-            end
+            WeakAuras.ScanEvents("SL_KEYBINDING_REGISTER", checkID, type)
         end
 
-        for i = 1, #ids do
-            if id == ids[i] then
-                allstates["KEY_BINDING"] = {
-                    show = true,
-                    changed = true,
-                    name = key,
-                    keyDown = aura_env.keyDown
-                }
-                aura_env.key = key
-                aura_env.keys = keys
+        if id == checkID then
+            allstates["KEY_BINDING"] = {
+                show = true,
+                changed = true,
+                name = key,
+                keyDown = aura_env.keyDown
+            }
+            aura_env.key = key
+            aura_env.keys = keys
 
-                if keys and next(keys) then
-                    WeakAuras.ScanEvents("SL_KEYDOWN_REGISTER", aura_env.id, aura_env.keys)
-                else
-                    WeakAuras.ScanEvents("SL_KEYDOWN_UNREGISTER", aura_env.id)
-                end
+            if keys and next(keys) then
+                WeakAuras.ScanEvents("SL_KEYDOWN_REGISTER", aura_env.id, aura_env.keys)
+            else
+                WeakAuras.ScanEvents("SL_KEYDOWN_UNREGISTER", aura_env.id)
             end
         end
 
